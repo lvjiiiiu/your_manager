f78c63598cbe73aea39aedb87f9f62f2869b9710
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-01-20 07:12:51 +0000

    devise機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
+  devise_for :users
+  root to: "homes#top"
+  get "home/about" => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
