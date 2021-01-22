990e91339e7c35075020ba36d9a0c0b74ba6e1dd
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-01-22 12:11:35 +0000

    タスクのCRUD機能完成(モーダルフォーム)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,26 +5,42 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
-    #respond_to do |format|
-    #format.html{}
-    #format.js {}
-    #end
+    @matrix = params[:matrix]
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.user_id = current_user.id
 
+    if params["task"][:matrix] == "1"
+      @task.task_matrix = "重要かつ緊急"
+    elsif params["task"][:matrix] == "2"
+      @task.task_matrix = "重要で緊急でない"
+    elsif params["task"][:matrix] == "3"
+      @task.task_matrix = "重要でなく緊急"
+    else
+      @task.task_matrix = "重要でないかつ緊急でない"
+    end
+    @task.save
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+    redirect_to tasks_path
   end
 
 
