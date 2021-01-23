a862b7fd09dbb5f0773c43dbd5d1d0366857ec6a
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-01-23 06:35:15 +0000

    [Fix]マトリクスごとのタスク一覧表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,11 @@
 class TasksController < ApplicationController
-  after_action :tasks_each_matrix, except: [:new, :index]
-  before_action :tasks_each_matrix, only: [:index]
+
 
 
   def index
-    #@tasks = Task.where(user_id: params[:user_id])
-
-    # @tasks.each do |task|
-    #   if task.task_matrix = "重要で緊急でない"
-    #     @task_important_no_urgent = task.where(task_matrix: "重要で緊急でない")
-    #   elsif task.task_matrix = "重要かつ緊急"
-    #     @task_important_urgent = task.where(task_matrix: "重要かつ緊急")
-    #   elsif task.task_matrix = "重要でなく緊急でない"
-    #     @task_no_important_no_urgent = task.where(task_matrix: "重要でなく緊急でない")
-    #   else
-    #     @task_no_important_urgent = task.where(task_matrix: "重要でなく緊急")
-    #   end
-    # end
-
+    tasks_each_matrix
   end
+
   def new
     @task = Task.new
     @matrix = params[:matrix]
@@ -33,11 +20,13 @@ class TasksController < ApplicationController
     elsif params["task"][:matrix] == "2"
       @task.task_matrix = "重要かつ緊急"
     elsif params["task"][:matrix] == "3"
-      @task.task_matrix = "重要でなく緊急でない"
+      @task.task_matrix = "重要でないかつ緊急でない"
     else
       @task.task_matrix = "重要でなく緊急"
     end
     @task.save
+    @tasks = Task.where(user_id: current_user)
+    tasks_each_matrix
   end
 
   def show
@@ -51,11 +40,15 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    @tasks = Task.where(user_id: current_user)
+    tasks_each_matrix
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.delete
+    @tasks = Task.where(user_id: current_user)
+    tasks_each_matrix
   end
 
 
@@ -70,7 +63,7 @@ class TasksController < ApplicationController
   def tasks_each_matrix
     @tasks_important_no_urgent = Task.where(user_id: params[:user_id], task_matrix: "重要で緊急でない")
     @tasks_important_urgent = Task.where(user_id: params[:user_id], task_matrix: "重要かつ緊急")
-    @tasks_no_important_no_urgent = Task.where(user_id: params[:user_id], task_matrix: "重要でなく緊急でない")
+    @tasks_no_important_no_urgent = Task.where(user_id: params[:user_id], task_matrix: "重要でないかつ緊急でない")
     @tasks_no_important_urgent = Task.where(user_id: params[:user_id], task_matrix: "重要でなく緊急")
   end
 end
