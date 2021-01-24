110cefe22589a65bad67cfd29107e859ab83204e
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-01-24 10:04:46 +0000

    ブランチ切り替え

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+
+
+    def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    @task = @comment.task
+    end
+
+  def destroy
+    task = Task.find(params[:id])
+    comment = task.comments.find(params[:id])
+    comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment, :task_id, :user_id)
+  end
 end
