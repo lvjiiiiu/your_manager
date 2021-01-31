0bcb4770c2c329044d715d31f545e9b155dfb281
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-01-31 22:44:35 +0000

    微修正

--- app/assets/javascripts/calendars.js
+++ app/assets/javascripts/calendars.js
@@ -1,29 +1,7 @@
 
 // フルcalendar
 
-// $(function () {
-//     eventCalendar();
-
-//   function eventCalendar() {
-//     return $('#calendar').fullCalendar({});
-//   };
-//   function clearCalendar() {
-//       $('#calendar').html('');
-//   };
-// });
-
-
 $(function () {
-  function eventCalendar() {
-    return $('#calendar').fullCalendar({});
-  };
-  function clearCalendar() {
-    $('#calendar').html('');
-  };
-  $(document).on('turbolinks:load', function () {
-  eventCalendar();
-  });
-  $(document).on('turbolinks:before-cache', clearCalendar);
 
   $('#calendar').fullCalendar({
     events: '/calendars.json',
@@ -33,81 +11,20 @@ $(function () {
     navLinks: true,
     selectable: true,
     selectHelper: true,
-    // 日付クリック
-    // dayClick : function ( date , jsEvent , view ) {
-    //   $('#inputScheduleForm').modal('show');
-    // },
+
     // event クリックで編集、削除
     eventClick : function(event, jsEvent , view) {
       jsEvent.preventDefault();
 
       $(`#inputScheduleEditForm${event.id}`).modal('show');
     },
-    // eventClick : function(event, jsEvent , view) {
-    //   var item = event.item;
-    //   var item_id = $(item).find('.item').attr('value')
-
-
-    //   $.ajax({
-    //   url: "calendar/" + task_item_id,
-    //   type: "get",
-    //   dataType: 'json',
-    // });
-    // }
 
     eventRender: function(event, element) {
       element.css("font-size", "0.8em");
       element.css("padding", "5px");
     },
 
-
-    // eventClick : function(event, jsEvent , view) {
-    //   jsEvent.preventDefault();
-    //   $(`#inputScheduleEditForm${event.id}`).modal('show');
-    // },
-
   });
 });
 
 
-
-
-
-
-
-
-
-
-
-// $('#calendar').fullCalendar ({
-//     header: {
-//         left: 'prev,next today',
-//         center: 'month,agendaWeek,agendaDay',
-//         right: 'title'
-//     },
-
-//     buttonText: {
-//           prev: "<",
-//           next: ">"
-//     },
-
-//     timezone: 'UTC',
-//     events: '/tasks.json',
-//     navLinks: true,
-//     selectable: true,
-//     selectHelper: true,
-//     // 日付クリック
-//     dayClick : function ( date , jsEvent , view ) {
-//         $('#task-modal').modal('show');
-//         },
-
-//     event クリックで編集、削除
-//     eventClick : function(event, jsEvent , view) {
-//         jsEvent.preventDefault();
-//         $(`#inputScheduleEditForm${event.id}`).modal('show');
-//     },
-
-//     eventMouseover : function(event, jsEvent , view) {
-//         jsEvent.preventDefault();
-//     }
-// })
