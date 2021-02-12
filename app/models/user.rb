0dce3f2734a301f1de32c7153c4843555dc65652
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-02-12 04:58:25 +0000

    最新masterをmerge後のrubocop追加自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,8 @@ class User < ApplicationRecord
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
-    user.password = SecureRandom.urlsafe_base64
-    user.name = "ゲスト"
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
     end
   end
-
 end
