3ac81046af1089e70e4652aa41c11a118de0549f
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-03-10 10:33:47 +0000

    [作成]task_spec(モデル)

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,6 +3,11 @@ FactoryBot.define do
     sequence(:task_title) { |n| "TASK#{n}"}
     sequence(:task_details) { |n| "TASK_DETAILS#{n}"}
     
-    user
+    Task.task_matrices.keys.each do |matrix|
+      trait :"#{matrix}" do
+        status {matrix}
+      end
+    end
+    association :user
   end
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  
+  describe 'バリデーションのテスト' do
+    let(:user) { create(:user) }
+    let!(:task) { build(:task, user_id: user.id) }
+
+    context 'titleカラム' do
+      it '空欄でないこと' do
+        task.task_title = ''
+        expect(task.valid?).to eq false;
+      end
+    end
+  end 
+  
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Task.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'Commentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Task.reflect_on_association(:comments).macro).to eq :has_many
+      end
+    end
+    context 'Notificationモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Task.reflect_on_association(:notifications).macro).to eq :has_many
+      end
+    end
+  end
+end
+
